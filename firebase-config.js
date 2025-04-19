9cc394fd25145a0ea6b4554a7240ca4cf405b7f0
Jvow03 <166693661+Jvow03@users.noreply.github.com>
2025-04-19 15:29:32 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+
+import {{ initializeApp }} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import {{ getAnalytics }} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
+
+const firebaseConfig = {{
+  apiKey: "AIzaSyCzxobtCQSe8DM1RYyNyR2pBxseqw0khz8",
+  authDomain: "teens-in-tech.firebaseapp.com",
+  projectId: "teens-in-tech",
+  storageBucket: "teens-in-tech.appspot.com",
+  messagingSenderId: "984870510539",
+  appId: "1:984870510539:web:708525d91cc67d11b7e04e",
+  measurementId: "G-74QPWHJXQ4"
+}};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ script.js
@@ -0,0 +1,5 @@
+
+setTimeout(() => {
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("content").style.display = "block";
+}, 5000);
